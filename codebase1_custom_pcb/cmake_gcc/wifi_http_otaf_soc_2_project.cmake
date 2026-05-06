7c09a7d48e0af1999b5e71fa0dd32c49fabe7074
Ananya Shivarama Bhat <ananyashivaramabhat@gmail.com>
2026-05-06 18:26:22 -0400

    Codebase link

--- codebase1_custom_pcb/cmake_gcc/wifi_http_otaf_soc_2_project.cmake
+++
@@ -1,3 +0,0 @@
-target_sources(wifi_http_otaf_soc_2 PRIVATE
-	"../otaf_app.c"
-)
--- codebase2_dev_board_arducam/cmake_gcc/wifi_http_otaf_camera_project.cmake
+++
@@ -1,9 +0,0 @@
-target_sources(wifi_http_otaf_camera PRIVATE
-	"../arducam.c"
-	"../image_proc.c"
-	"../mqtt_pub.c"
-	"../ov2640.c"
-	"../pill_counter.c"
-	"../sensor_i2c.c"
-	"../tjpgd.c"
-)
